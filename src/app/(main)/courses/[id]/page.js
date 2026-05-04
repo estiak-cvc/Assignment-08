60d7e83c8002fdddf5eb087753892da04afe0fa7
estiak-cvc <confidenc.esth@gmail.com>
2026-05-04 08:19:13 +0600

    Detail page1

--- src/app/(main)/courses/[id]/page.js
+++ src/app/(main)/courses/[id]/page.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const DetailsPage = async({params}) => {
     const {id} = await params;
+    const res = await fetch('https://assignment-08-xi-one.vercel.app/data/courses.json')
+    const courses = await res.json()
+
+    const course = courses.find(c => c.id == id)
     return (
         <div>
-            Courses Details Page
+            <h1>{course?.title}</h1>
+            <p>{course.instructor} </p>
         </div>
     );
 };
